982cc91febf812a8707f1ec8d09c21413df15cd8
Reteriene <Rodrigoeteriene@gmail.com>
2025-10-09 10:28:11 -0300

    TechFusion

---
+++ TechFusion-web/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+ import cabecalho from './componentes/header.vue';
+
+
+
+</script>
+
+<template>
+  <cabecalho> </cabecalho>
+
+
+
+
+
+
+
+
+</template>
+
+<style scoped></style>
